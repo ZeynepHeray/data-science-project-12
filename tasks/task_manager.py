c465a043dd873bafc9d921104446229042cde2f3
Zeynep Heray <zeynepheray2001@gmail.com>
2025-07-31 11:12:42 +0300

    cevaplar

--- tasks/task_manager.py
+++ tasks/task_manager.py
@@ -2,57 +2,65 @@
 # Input: DataFrame
 # Output: Series (Chemical isimleri)
 def get_hazardous_chemicals(df):
-    pass
+    return df[df['IsHazardous']==True]
 
 
 # Tüm "Amount" değerlerini grama çevir. (1 liter = 1000 gram, 1 kg = 1000 gram)
 # Input: DataFrame
 # Output: Yeni DataFrame, "Amount" sütunu gram cinsinden.
 def convert_amounts_to_grams(df):
-    pass
+    df['Amount']= df['Amount']*1000
+    return df
 
 
 # Miktarı en fazla olan n kimyasalı döndür (gram cinsinden sıralı).
 # Input: DataFrame, n=2
 # Output: En fazla miktarda 2 kimyasal.
 def get_top_n_chemicals(df, n):
-    pass
+    sirali = df.sort_values('Amount',ascending=False)
+    return sirali.head(n)
 
 
 # Kullanılan birimlerin listesini döndür ("liter", "kg" vb.)
 # Input: DataFrame
 # Output: Series ya da list
 def get_unique_units(df):
-    pass
+    return df['Unit']
 
 
 #  İsim içerisinde keyword geçen kimyasalları filtrele.
 # Input: keyword="Acetone"
 # Output: "Acetone" içeren satırları içeren DataFrame
 def filter_chemicals_by_name(df, keyword):
-    pass
+    return df[df['Chemical']==keyword]
 
 
 # Toplam madde miktarını gram cinsinden hesapla.
 # Output: float
 def get_total_amount(df):
-    pass
+    return df['Amount'].sum()
 
 
 # NumPy kullanarak miktarların standart sapmasını hesapla.
 # Output: float
 def calculate_standard_deviation(df):
-    pass
+    import numpy as np
+    std=np.std(df['Amount'])
+    return std
 
 
 # Miktarları min-max normalize et.
 # (formül: (x - min) / (max - min))
 # Output: Series (normalize edilmiş değerler)
 def normalize_amounts(df):
-   pass
+   mini=df['Amount'].min()
+   maxi=df['Amount'].max()
+   val= (df['Amount']-mini)/(maxi-mini)
+   return val
 
 
 # Tehlikeli olup miktarı 1000 gramdan fazla olanları "HighRisk" olarak işaretle.
 # Output: Yeni DataFrame, HighRisk adında yeni sütun içerir.
 def flag_high_risk(df):
-    pass
+    df['HighRisk'] = df['Amount'].apply(lambda x : x>1000 )
+    return df
